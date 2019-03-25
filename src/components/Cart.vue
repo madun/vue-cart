6349924ed18ec6bf6a80f9102da4d7da3b9c2c95
madungit remote origigit config --global user.name madun <ilmankori@gmail.com>
2019-03-25 20:35:37 +0700

    Done

---
+++ src/components/Cart.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul class="list-group mt-4">
+            <li class="list-group-item" v-for="item in items" :key="item.id">
+                {{ item.title }} - ${{ item.price }}
+                <button class="btn badge badge-danger float-right"
+                @click="$emit('remove-from-cart', item)">Remove</button>
+            </li>
+        </ul>
+        <div v-if="items.length !== 0 ">
+            <div class="card p-3 my-5">
+                <h4 class="text-center">${{ total }}</h4>
+            </div>
+            <button class="btn btn-info form-control"
+                @click="$emit('pay')"
+            >Pay Now</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['items'],
+    computed: {
+     total(){
+         return this.items.reduce((acc, item) => acc + Number(item.price), 0)
+     }   
+    }
+}
+</script>
+
---
+++ src/components/Product.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card mt-4">
+        <img class="card-img-top" :src="product.image">
+        <div class="card-body">
+            <h5 class="text-center card-title">{{ product.title }}</h5>
+            <p class="text-center text-muted card-text display-4">$ {{ Number(product.price).toFixed() }}</p>
+        </div>
+        <div class="card-footer text-center">
+            <button class="btn btn-success"
+                :disabled="isInCart"
+                @click="$emit('add-to-cart', product)">
+            {{ isInCart ? 'Added To Cart' : 'Add To Cart' }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['product', 'isInCart']
+}
+</script>
+
